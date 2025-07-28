e4a197ce3995b44e3604544462cbba169c4316be
Kirill Berezhnoy <k.berezhnoy@vkteam.ru>
2025-07-28 15:18:50 +0300

    IOS-5 add custom colors to project

---
+++ CommonUI/CommonUI/Resources/Colors/Colors.swift
@@ -0,0 +1,54 @@
+import SwiftUI
+
+public extension Color {
+
+    // MARK: - Статические цвета
+
+    static let purple700 = Color(hex: "#6A0BFF")
+    static let black900 = Color(hex: "#191919")
+    static let black800 = Color(hex: "#303030")
+    static let black700 = Color(hex: "#474747")
+    static let black50 = Color(hex: "#808080")
+    static let black10 = Color(hex: "#F5F5F5")
+    static let pureWhite = Color(hex: "#FFFFFF")
+
+    // MARK: - Адаптивные цвета (инверсия для темной темы)
+
+    static let backgroundPrimary = adaptiveColor(light: .pureWhite, dark: .black900)
+    static let backgroundSecondary = adaptiveColor(light: .black10, dark: .black900.opacity(0.8))
+    static let textPrimary = adaptiveColor(light: .black900, dark: .pureWhite)
+    static let textSecondary = adaptiveColor(light: .black50, dark: .black10)
+
+    // MARK: - Вспомогательные методы
+
+    private static func adaptiveColor(light: Color, dark: Color) -> Color {
+        #if canImport(UIKit)
+        return Color(UIColor { trait in
+            trait.userInterfaceStyle == .dark ? UIColor(dark) : UIColor(light)
+        })
+        #else
+        return light // Для macOS используем только светлую тему
+        #endif
+    }
+
+}
+
+// MARK: - Конвертация для UIKit
+
+extension UIColor {
+
+    convenience init(_ color: Color) {
+        self.init(color, dynamicProvider: nil)
+    }
+
+    convenience init(_ color: Color, dynamicProvider: ((UITraitCollection) -> UIColor)? = nil) {
+        if let dynamicProvider = dynamicProvider {
+            self.init { trait in
+                dynamicProvider(trait)
+            }
+        } else {
+            self.init(color)
+        }
+    }
+
+}
