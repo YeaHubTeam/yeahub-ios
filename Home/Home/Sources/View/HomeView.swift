af3fb08fcdbc580ae3bda8ea9159194c98fe885a
Alex <sangawere1@gmail.com>
2025-07-30 20:17:12 +0300

    Fix main screen button paddings

--- Home/Home/Sources/View/HomeView.swift
+++ Home/Home/Sources/View/HomeView.swift
@@ -121,7 +121,7 @@ struct HomeView: View {
                 Text(description)
                     .foregroundStyle(Color.black900)
                     .font(.manrope(.medium, size: 16))
-                    .frame(maxWidth: Constants.Button.descriptionMaxWidth, alignment: .leading)
+                    .frame(maxWidth: .infinity, alignment: .leading)
                     .foregroundColor(.secondary)
             }
         }
@@ -138,8 +138,7 @@ extension HomeView {
 
         enum Button {
             static let horizontalInset: CGFloat = 12
-            static let verticalinset: CGFloat = 8
-            static let descriptionMaxWidth: CGFloat = 222
+            static let verticalinset: CGFloat = 4
             static let cornerRadius: CGFloat = 8
         }
 
@@ -150,7 +149,7 @@ extension HomeView {
             static let cornerRadius: CGFloat = 10
         }
 
-        static let gridSpacing: CGFloat = 8
+        static let gridSpacing: CGFloat = 4
         static let defaultInset: CGFloat = 16
         static let topInset: CGFloat = 24
         static let textsHeaderInset: CGFloat = 8
