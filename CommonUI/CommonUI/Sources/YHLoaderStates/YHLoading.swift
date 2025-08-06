6e01a48dd905b27b1946916cd7c03bde5257bf4f
Федор Задорожный <fedor@MacBook-Pro.local>
2025-08-06 20:05:32 +0300

    YHIOS-31: Add Constants for YHLoading

--- CommonUI/CommonUI/Sources/YHLoaderStates/YHLoading.swift
+++ CommonUI/CommonUI/Sources/YHLoaderStates/YHLoading.swift
@@ -5,8 +5,6 @@ public struct YHLoading: View {
     let title: String
 
     @State private var isAnimating = false
-    private let dotSize: CGFloat = 12
-    private let animationDuration: Double = 0.6
 
     public init(title: String, isAnimating: Bool = false) {
         self.title = title
@@ -14,41 +12,60 @@ public struct YHLoading: View {
     }
 
     public var body: some View {
-        VStack(spacing: 16) {
-            HStack(spacing: 8) {
-                ForEach(0..<3, id: \.self) { index in
+        VStack(spacing: Constants.vStackSpacing) {
+            HStack(spacing: Constants.hStackSpacing) {
+                ForEach(Constants.dots, id: \.self) { index in
                     Circle()
                         .fill(Color.purple700)
-                        .frame(width: dotSize, height: dotSize)
-                        .scaleEffect(isAnimating ? 1.0 : 0.5)
+                        .frame(width: Constants.dotSize, height: Constants.dotSize)
+                        .scaleEffect(isAnimating ? Constants.scaleMaxSize : Constants.scaleMinSize)
                         .animation(
-                            Animation.easeInOut(duration: animationDuration)
+                            Animation.easeInOut(duration: Constants.animationDuration)
                                 .repeatForever()
-                                .delay(Double(index) * animationDuration / 3),
+                                .delay(Double(index) * Constants.delay),
                             value: isAnimating
                         )
                 }
             }
 
-            // Текст
             Text(title)
-                .font(.manrope(.medium, size: 14))
+                .font(.manrope(.medium, size: Constants.titleFontSize))
                 .foregroundColor(.black900)
         }
-        .padding(24)
-        .frame(width: 156)
+        .padding(Constants.defaultPadding)
+        .frame(width: Constants.roundedRectangleWidth)
         .background(
-            RoundedRectangle(cornerRadius: 12)
+            RoundedRectangle(cornerRadius: Constants.roundedRectangleCornerRadius)
                 .fill(Color.pureWhite)
-                .shadow(
-                    color: Color(hex: "#6A6376").opacity(0.1),
-                    radius: 10,
-                    x: 0,
-                    y: 4
-                )
+                .defaultShadow()
         )
         .onAppear {
             isAnimating = true
         }
     }
+}
+
+//MARK: - Constants
+private extension YHLoading {
+    enum Constants {
+        static let vStackSpacing: CGFloat = 16
+        static let hStackSpacing: CGFloat = 8
+
+        static let dots = Array(0..<3)
+        static let dotSize: CGFloat = 12
+        static let animationDuration: Double = 0.6
+        static let delay: Double = animationDuration / Double(dots.count)
+        static let scaleMinSize: CGFloat = 0.5
+        static let scaleMaxSize: CGFloat = 1
+
+        static let titleFontSize: CGFloat = 14
+
+        static let defaultPadding: CGFloat = 24
+        static let roundedRectangleWidth: CGFloat = 156
+        static let roundedRectangleCornerRadius: CGFloat = 12
+    }
+}
+
+#Preview {
+    YHLoading(title: "Loading…")
 }
