801679ae7a19c54ee13d12250978e65f6e148272
Федор Задорожный <fedor@MacBook-Pro.local>
2025-08-08 19:01:13 +0300

    YUHIOS-3: Custom YHTextField has been added

---
+++ CommonUI/CommonUI/Sources/YHTextField.swift
@@ -0,0 +1,47 @@
+import SwiftUI
+
+public struct YHTextField: View {
+    private let title: String
+    @Binding private var text: String
+    @FocusState private var isFocused: Bool
+
+    public init(
+        title: String = "Поиск",
+        text: Binding<String>,
+    ) {
+        self.title = title
+        self._text = text
+    }
+
+    public var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: Constants.cornerRadius)
+                .stroke(isFocused ? Color.purple700 : Color.black50)
+                .foregroundStyle(isFocused ? Color.black25 : Color.pureWhite)
+                .frame(height: Constants.height)
+                .padding(Constants.rectanglePadding)
+
+            HStack(spacing: Constants.hStackSpacing) {
+                CommonUIAssets.image(Constants.imageName)
+                    .foregroundColor(isFocused ? Color.black900 : Color.black300)
+
+                TextField(title, text: $text)
+                    .focused($isFocused)
+                    .font(Font.manrope())
+                    .submitLabel(.search)
+            }
+            .padding(Constants.hStackPadding)
+        }
+    }
+}
+
+private extension YHTextField {
+    enum Constants {
+        static let cornerRadius: CGFloat = 12
+        static let height: CGFloat = 48
+        static let rectanglePadding: CGFloat = 16
+        static let hStackSpacing: CGFloat = 16
+        static let hStackPadding: CGFloat = 32
+        static let imageName = "magnifer"
+    }
+}
