39f5cdc99011581091ea91898cd701eb94c4c1e3
aachekin <aachekin@avito.ru>
2025-08-13 23:08:31 +0300

    YHIOS-30: icons moved to CommonUIAssets

--- YeaHub/YeaHub/Resources/UIImage+Extension.swift
+++
@@ -1,7 +0,0 @@
-import UIKit
-
-public extension UIImage {
-    
-    static let homeVCImageTabBarLogo = UIImage(named: "HomeIcon")
-    static let collectionsVCImageTabBarLogo = UIImage(named: "CollectionsIcon")
-}
--- YeaHub/YeaHub/Sources/Coordinators/Main/MainCoordinatorImpl.swift
+++ YeaHub/YeaHub/Sources/Coordinators/Main/MainCoordinatorImpl.swift
@@ -52,7 +52,7 @@ class MainCoordinatorImpl: BaseCoordinator, MainCoordinator {
         // Настраиваем табы
         homeVC.tabBarItem = UITabBarItem(
             title: "Главная",
-            image: .homeVCImageTabBarLogo,
+            image: CommonUIAssets.homeVCImageTabBarLogo,
             tag: 0
         )
 
@@ -65,7 +65,7 @@ class MainCoordinatorImpl: BaseCoordinator, MainCoordinator {
 
         collectionsVC.tabBarItem = UITabBarItem(
             title: "Коллекции",
-            image: .collectionsVCImageTabBarLogo,
+            image: CommonUIAssets.collectionsVCImageTabBarLogo,
             tag: 0
         )
 
