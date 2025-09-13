f38aeb3a4b2c90c3831d501604717bccc3e05a04
Федор Задорожный <fedor@MacBook-Pro.local>
2025-09-13 14:19:10 +0300

    YHIOS-20: The Constants for SpecializationsViewController has been added

--- SharedScreens/SharedScreens/Questions/Specializations/View/SpecializationsViewController.swift
+++ SharedScreens/SharedScreens/Questions/Specializations/View/SpecializationsViewController.swift
@@ -27,17 +27,14 @@ public class SpecializationsViewController: UIViewController {
     }
 
     private func setupNavigationBar() {
-        title = "Выбор специальности"
+        title = Constants.title
 
-        let appearance = UINavigationBarAppearance()
-
-        appearance.titleTextAttributes = [
-            .foregroundColor: UIColor(.black900),
-            .font: UIFont.manrope(.medium, size: 16)
+        navigationController?.navigationBar.titleTextAttributes = [
+            .foregroundColor: Constants.navigationBarFont,
+            .font: Constants.navigationBarFont
         ]
-
+        
         navigationController?.navigationBar.tintColor = UIColor(.purple700)
-
     }
 
     private func setupSwiftUIView() {
@@ -58,5 +55,13 @@ public class SpecializationsViewController: UIViewController {
         ])
 
         hostingController.view.backgroundColor = .clear
+    }
+}
+
+private extension SpecializationsViewController {
+    enum Constants {
+        static let title = "Выбор специальности"
+        static let navigationBarFont = UIFont.manrope(.medium, size: 16)
+        static let navigationBarForegroundColor = UIColor(.black900)
     }
 }
