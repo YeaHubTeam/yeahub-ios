19aa66aa534212d68323bb9e2716ce298885d622
Alex <sangawere1@gmail.com>
2025-09-15 15:28:12 +0300

    Remove duplicate QuestionsSelectionScreens folder

--- SharedScreens/SharedScreens/QuestionsSelectionScreens/Source/Coordinator/QuestionsListCoordinator.swift
+++
@@ -1,8 +0,0 @@
-import NavigationKit
-
-public protocol QuestionsListCoordinator: Coordinator {
-    var factory: QuestionsListFactory { get }
-    
-    func start(with specializationId: Int, specializationTitle: String)
-    func getQuestionsListScreen() -> QuestionsListViewController?
-}
--- SharedScreens/SharedScreens/QuestionsSelectionScreens/Source/Factory/QuestionsListFactory.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-import NavigationKit
-
-public protocol QuestionsListFactory {
-    func makeQuestionsListScreen(specializationId: Int, specializationTitle: String) -> QuestionsListViewController
-}
