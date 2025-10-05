574a61b1f4856ca4e38d050e03ff73c46ca7929d
foxsetDev <foxsetned@gmail.com>
2025-10-06 00:47:37 +0300

    [YHIOS-58]: Add YHBubbleCollectionLayout

---
+++ CommonUI/CommonUI/Sources/YHBubbleCollection/Layout/YHBubbleCollectionLayout.swift
@@ -0,0 +1,81 @@
+import SwiftUI
+
+struct YHBubbleCollectionLayout: Layout {
+    var spacing: CGFloat = 12
+    var maxItemWidth: CGFloat? = nil
+    
+    func makeCache(subviews: Subviews) -> [CGSize] {
+        Array(repeating: .zero, count: subviews.count)
+    }
+    
+    func sizeThatFits(
+        proposal: ProposedViewSize,
+        subviews: Subviews,
+        cache: inout [CGSize]
+    ) -> CGSize {
+        var width: CGFloat = 0
+        var height: CGFloat = 0
+        var lineHeight: CGFloat = 0
+        let maxWidth = proposal.width ?? .infinity
+        let itemMaxWidth = maxItemWidth ?? maxWidth
+        
+        for (index, subview) in subviews.enumerated() {
+            if cache[index] == .zero {
+                cache[index] = subview.sizeThatFits(
+                    ProposedViewSize(
+                        width: itemMaxWidth,
+                        height: nil
+                    )
+                )
+            }
+            let size = cache[index]
+            
+            if width + size.width > maxWidth {
+                width = 0
+                height += lineHeight + spacing
+                lineHeight = 0
+            }
+            
+            width += size.width + spacing
+            lineHeight = max(lineHeight, size.height)
+        }
+        
+        height += lineHeight
+        return CGSize(
+            width: maxWidth,
+            height: height
+        )
+    }
+    
+    func placeSubviews(
+        in bounds: CGRect,
+        proposal: ProposedViewSize,
+        subviews: Subviews,
+        cache: inout [CGSize]
+    ) {
+        var x: CGFloat = bounds.minX
+        var y: CGFloat = bounds.minY
+        var lineHeight: CGFloat = 0
+        
+        for (index, subview) in subviews.enumerated() {
+            let size = cache[index]
+            
+            if x + size.width > bounds.width {
+                x = bounds.minX
+                y += lineHeight + spacing
+                lineHeight = 0
+            }
+            
+            subview.place(
+                at: CGPoint(x: x, y: y),
+                proposal: ProposedViewSize(
+                    width: size.width,
+                    height: size.height
+                )
+            )
+            
+            x += size.width + spacing
+            lineHeight = max(lineHeight, size.height)
+        }
+    }
+}
