c11b47222a0547f03408b20ffff58fc30dab01a6
Alex <sangawere1@gmail.com>
2025-10-18 13:18:31 +0300

    ⁠bugfix/YHIOS-81-fix-navBar-in-collections-screen

--- Collections/Collections/Source/Specializations/View/SpecializationsViewController.swift
+++ Collections/Collections/Source/Specializations/View/SpecializationsViewController.swift
@@ -48,7 +48,7 @@ public class SpecializationsViewController: UIViewController {
         hostingController.view.translatesAutoresizingMaskIntoConstraints = false
 
         NSLayoutConstraint.activate([
-            hostingController.view.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            hostingController.view.topAnchor.constraint(equalTo: view.topAnchor),
             hostingController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             hostingController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             hostingController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor)
