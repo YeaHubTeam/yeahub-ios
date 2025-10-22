6a063f58fb01f248e2f210d435a45748958ee7ba
foxsetDev <foxsetned@gmail.com>
2025-10-23 02:18:13 +0300

    [YHIOS-99]: New icon - Profile
    Новая Иконка для ТабБара - Профиль

--- CommonUI/CommonUI/Resources/CommonUIAssets.swift
+++ CommonUI/CommonUI/Resources/CommonUIAssets.swift
@@ -7,6 +7,7 @@ public enum CommonUIAssets {
         Image(name, bundle: bundle)
     }
     
+    public static let profileVCImageTabBarLogo = UIImage(named: "ProfileIcon")
     public static let homeVCImageTabBarLogo = UIImage(named: "HomeIcon")
     public static let questionsVCImageTabBarLogo = UIImage(named: "QuestionIcon")
     public static let collectionsVCImageTabBarLogo = UIImage(named: "CollectionsIcon")
