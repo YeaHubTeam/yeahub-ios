952d80645c4bf7776819852794b12ec5ab35f371
foxsetDev <foxsetned@gmail.com>
2025-10-28 01:03:17 +0300

    [YHIOS-80]: refactor TagItem identifier using API
    - была проблема в TagItem.id генерировал случайный UUID() при каждой инициализации
    - TagItem: теперь принимает id из данных API (Int)

--- CommonUI/CommonUI/Sources/YHBubbleCollection/Model/YHBubbleTagItem.swift
+++ CommonUI/CommonUI/Sources/YHBubbleCollection/Model/YHBubbleTagItem.swift
@@ -1,14 +1,16 @@
 import Foundation
 
 public struct TagItem: Identifiable, Hashable {
-    public let id = UUID()
+    public let id: Int
     public let title: String
     public let icon: YHBubbleTagIcon?
     
     public init(
+        id: Int,
         title: String,
         icon: YHBubbleTagIcon? = nil
     ) {
+        self.id = id
         self.title = title
         self.icon = icon
     }
