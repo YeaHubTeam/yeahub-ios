bdcb793bbb91792a294866c420b64a98928f60e0
foxsetDev <foxsetned@gmail.com>
2025-10-29 20:30:11 +0300

    [YHIOS-80]: migrate-to-multi-specialization-flow

--- SharedScreens/SharedScreens/QuestionModuleScreens/Specializations/Model/Specialization.swift
+++ SharedScreens/SharedScreens/QuestionModuleScreens/Specializations/Model/Specialization.swift
@@ -1,5 +1,5 @@
 public struct Specialization: Decodable, Hashable, Identifiable {
     public let id: Int
-    let title: String
-    let description: String
+    public let title: String
+    public let description: String
 }
